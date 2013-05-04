c04fed1bd5f849f46c92c2482b1835abc69f7add
Michal Madr <abc@gmail.com>
2013-05-04 06:55:35 +0200

    initial version

---
+++ src/cpp/a.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iterator>
+
+using namespace std;
+
+
+
+int main(int argc, char** argv)
+{
+   cout << "OK" << endl;
+}
+
